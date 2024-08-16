30a0bad933aa863d688c2a892b0d6d960315de31
José Honorio de Oliveira Sobrinho Neto <honorio.dev@gmail.com>
2024-08-16 18:23:27 -0300

    add initial config nextjs

---
+++ src/app/layout.tsx
@@ -0,0 +1,11 @@
+interface RootLayoutProps {
+  children: React.ReactNode;
+}
+
+export default function RootLayout({ children }: RootLayoutProps) {
+  return (
+    <html lang="pt-br">
+      <body>{children}</body>
+    </html>
+  );
+}
